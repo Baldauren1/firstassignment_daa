83558c5737461fbe9f1339cac7b794f924852d20
Baldauren <241588@astanait.edu.kz>
2025-09-28 11:32:23 +0600

    fix(quicksort): fixed partition and recursion

--- src/main/java/org/example/sort/QuickSort.java
+++ src/main/java/org/example/sort/QuickSort.java
@@ -11,38 +11,49 @@ public class QuickSort {
         ArrayUtil.shuffle(a, rnd);
         sort(a,0,a.length-1);
     }
-    private void sort(int[] a, int l, int r){
-        while(l<r){
+    private void sort(int[] a, int l, int r) {
+        while (l < r) {
             m.enter();
-            if(r-l+1<=16){
-                for(int i=l;i<=r;i++){
-                    for(int j=i;j>l;j--){
+            if (r - l + 1 <= 16) {
+                // insertion sort
+                for (int i = l; i <= r; i++) {
+                    for (int j = i; j > l; j--) {
                         m.comparisons++;
-                        if(a[j]<a[j-1]){ int t=a[j]; a[j]=a[j-1]; a[j-1]=t; m.swaps++; } else break;
+                        if (a[j] < a[j - 1]) {
+                            int t = a[j]; a[j] = a[j - 1]; a[j - 1] = t; m.swaps++;
+                        } else break;
                     }
                 }
                 m.exit();
                 return;
             }
-            int p = partition(a,l,r);
-            if(p-l < r-p){
-                sort(a,l,p-1);
-                l = p+1;
+
+            int p = partition(a, l, r);
+
+            if (p - l < r - p) {
+                sort(a, l, p);
+                l = p + 1;
             } else {
-                sort(a,p+1,r);
-                r = p-1;
+                sort(a, p + 1, r);
+                r = p;
             }
             m.exit();
         }
     }
-    private int partition(int[] a, int l, int r){
-        int pivot = a[l + rnd.nextInt(r-l+1)];
-        int i = l, j = r;
-        while(i<=j){
-            while(i<=j){ m.comparisons++; if(a[i]<pivot) i++; else break; }
-            while(i<=j){ m.comparisons++; if(a[j]>pivot) j--; else break; }
-            if(i<=j){ int t=a[i]; a[i]=a[j]; a[j]=t; m.swaps++; i++; j--; }
+
+
+    private int partition(int[] a, int l, int r) {
+        int pivot = a[l + rnd.nextInt(r - l + 1)];
+        int i = l - 1;
+        int j = r + 1;
+        while (true) {
+            do { i++; m.comparisons++; } while (a[i] < pivot);
+            do { j--; m.comparisons++; } while (a[j] > pivot);
+            if (i >= j) {
+                return j;
+            }
+            int t = a[i]; a[i] = a[j]; a[j] = t;
+            m.swaps++;
         }
-        return i-1;
     }
 }
